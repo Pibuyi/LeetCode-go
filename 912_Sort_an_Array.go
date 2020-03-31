f2bd9eecd2d72e994cbaa2df7c1454f3635a5aaa
Pibuyi <33181512+Pibuyi@users.noreply.github.com>
2020-03-31 18:21:29 +0800

    Create 912_Sort_an_Array.go

---
+++ 912_Sort_an_Array.go
@@ -0,0 +1,15 @@
+func sortArray(nums []int) []int {
+    s := [100010] int{}
+    for i := 0; i < len(nums); i++ {
+        s[nums[i] + 50000]++
+    }
+    index := 0 
+    for i := 0; i < 100001; i++ {
+        for s[i] > 0 {
+            nums[index] = i - 50000
+            index++
+            s[i]--
+        }
+    }
+    return nums
+}
