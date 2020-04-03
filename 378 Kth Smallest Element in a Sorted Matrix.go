0734a51b25a9571557e5e2aac69248e668000479
Pibuyi <33181512+Pibuyi@users.noreply.github.com>
2020-04-03 19:56:22 +0800

    Create 378 Kth Smallest Element in a Sorted Matrix.go

---
+++ 378 Kth Smallest Element in a Sorted Matrix.go
@@ -0,0 +1,35 @@
+type IntHeap []int
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) {
+    // Push and Pop use pointer receivers because they modify the slice's length,
+    // not just its contents.
+    *h = append(*h, x.(int))
+}
+func (h *IntHeap) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+}
+
+func kthSmallest(matrix [][]int, k int) int {
+    h := &IntHeap{}
+    heap.Init(h)
+    for i := 0; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[0]); j++ {
+            heap.Push(h, matrix[i][j])
+            if h.Len() > k {                
+                heap.Pop(h)
+            }
+        }
+    }
+    var res int
+    switch v := heap.Pop(h).(type) {
+        case int:
+            res = v
+	}
+    return res
+}
