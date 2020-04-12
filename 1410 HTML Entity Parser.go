774fa5da1c9fb3294b164a7c1373af76047b0b19
Pibuyi <33181512+Pibuyi@users.noreply.github.com>
2020-04-12 18:57:36 +0800

    Update 1410 HTML Entity Parser.go

--- 1410 HTML Entity Parser.go
+++ 1410 HTML Entity Parser.go
@@ -1,9 +1,9 @@
 func entityParser(text string) string {
     text = strings.Replace(text, "&quot;", "\"",  -1)
     text = strings.Replace(text, "&apos;", "'",  -1)
-    text = strings.Replace(text, "&amp;", "&",  -1)
     text = strings.Replace(text, "&gt;", ">",  -1)
     text = strings.Replace(text, "&lt;", "<", -1)
     text = strings.Replace(text, "&frasl;", "/", -1)
+    text = strings.Replace(text, "&amp;", "&",  -1)
     return text
 }
